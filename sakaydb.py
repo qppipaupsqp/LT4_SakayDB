13f2ebbbdb5b0927e98ec09b8a2e99150c32ee8b
Stevie Barot <steviebarot@gmail.com>
2022-09-01 22:17:35 +0800

    Added methods and LTmate assignments.

--- sakaydb.py
+++ sakaydb.py
@@ -1,1 +1,41 @@
 # LT4 Sakay DB File
+
+class SakayDB:
+    def __init__(self):
+        pass
+        # Stevie
+
+    class SakayDBError(ValueError):
+        pass
+
+    def add_trip(self):
+        pass
+        # Stevie
+
+    def add_trips(self):
+        pass
+        # Stevie
+
+    def delete_trip(self):
+        pass
+        # Lenna
+
+    def search_trips(self):
+        pass
+        # Ken
+
+    def export_data(self):
+        pass
+        # Ranna
+
+    def generate_statistics(self):
+        pass
+        # Ben, AJ
+
+    def plot_statistics(self):
+        pass
+        # Ben, AJ
+
+    def generate_odmatrix(self):
+        pass
+        # Ben, AJ, Ken
